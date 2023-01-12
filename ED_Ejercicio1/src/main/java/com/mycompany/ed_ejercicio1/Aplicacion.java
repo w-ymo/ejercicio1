d3cc65d1b271b4eecf393be9e7f502b667ef6527
w-ymo <noel.prieto.uva@gmail.com>
2023-01-12 17:51:40 +0100

    Segundo Commit

---
+++ ED_Ejercicio1/src/main/java/com/mycompany/ed_ejercicio1/Aplicacion.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ed_ejercicio1;
+
+/**
+ *
+ * @author Peripito
+ */
+public class Aplicacion {
+
+    public static void main(String[] args) {
+        Votacion v = new Votacion("Delegado", 7);
+        v.toString();
+    }
+}
